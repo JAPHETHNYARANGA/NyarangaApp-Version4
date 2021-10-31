1f071d466e42bc1ca0f730a5f8415dfe21d6d24b
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-31 11:07:51 +0300

    Added cloud storage dependency

--- app/src/main/java/com/moringaschool/nyaranga_social_app_version2/controller/ui/ProfileActivity.java
+++ app/src/main/java/com/moringaschool/nyaranga_social_app_version2/controller/ui/ProfileActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.moringaschool.nyaranga_social_app_version2.R;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    private StorageReference mstorageRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+       mstorageRef = FirebaseStorage.getInstance().getReference();
     }
 }
