fcfd708c0c039ea42d140b41d60de36283c946c0
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-31 11:21:47 +0300

    created the upload image method and the onActivity Result

--- app/src/main/java/com/moringaschool/nyaranga_social_app_version2/controller/ui/ProfileActivity.java
+++ app/src/main/java/com/moringaschool/nyaranga_social_app_version2/controller/ui/ProfileActivity.java
@@ -1,8 +1,13 @@
 package com.moringaschool.nyaranga_social_app_version2.controller.ui;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -23,6 +28,18 @@ public class ProfileActivity extends AppCompatActivity {
        myimage = findViewById(R.id.myimage);
     }
     public void uploadImage(View v){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,101);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == 101){
+                Log.e("error","3");
+                onCaptureImageResult(data);
+            }
+        }
     }
 }
