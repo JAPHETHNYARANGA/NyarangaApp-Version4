643c716b368008812068c111367f0aa3eac24cf3
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-31 10:25:37 +0300

    Created profile Activity

---
+++ app/src/main/java/com/moringaschool/nyaranga_social_app_version2/controller/ui/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.nyaranga_social_app_version2.controller.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.nyaranga_social_app_version2.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
