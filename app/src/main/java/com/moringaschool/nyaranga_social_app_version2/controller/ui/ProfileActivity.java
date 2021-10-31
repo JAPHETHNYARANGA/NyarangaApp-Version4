14c1738b46e8e65794c04411a82d6fa1c1ac8d53
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-31 11:14:40 +0300

    Declared variables in the Profile Activity.java file

--- app/src/main/java/com/moringaschool/nyaranga_social_app_version2/controller/ui/ProfileActivity.java
+++ app/src/main/java/com/moringaschool/nyaranga_social_app_version2/controller/ui/ProfileActivity.java
@@ -3,6 +3,7 @@ package com.moringaschool.nyaranga_social_app_version2.controller.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -10,11 +11,15 @@ import com.moringaschool.nyaranga_social_app_version2.R;
 
 public class ProfileActivity extends AppCompatActivity {
     private StorageReference mstorageRef;
+    ImageView myimage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
        mstorageRef = FirebaseStorage.getInstance().getReference();
+
+       myimage = findViewById(R.id.myimage);
     }
+
 }
