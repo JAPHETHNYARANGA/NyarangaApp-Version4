4644031d8301e2f8bfe9623921d3c76cbd025343
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-10-31 11:44:54 +0300

    created Toast to show success or failure to upload

--- app/src/main/java/com/moringaschool/nyaranga_social_app_version2/controller/ui/ProfileActivity.java
+++ app/src/main/java/com/moringaschool/nyaranga_social_app_version2/controller/ui/ProfileActivity.java
@@ -75,6 +75,6 @@ public class ProfileActivity extends AppCompatActivity {
             public void onFailure(@NonNull  Exception e) {
                 Toast.makeText(ProfileActivity.this,"Failed to Upload", Toast.LENGTH_SHORT).show();
             }
-        })
+        });
     }
 }
